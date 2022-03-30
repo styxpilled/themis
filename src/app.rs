8b0461b1523e3e254fc8829312916e4984c72557
styxpilled <styxpilled.dev@gmail.com>
2022-03-30 02:13:01 +0200

    feat: scrolling

--- src/app.rs
+++ src/app.rs
@@ -174,109 +174,108 @@ impl epi::App for App {
     });
 
     egui::CentralPanel::default().show(ctx, |ui| {
-      egui::Grid::new("central_grid").show(ui, |ui| {
-        // * Breadcrumb navigation
-        ui.horizontal(|ui| {
-          ui.spacing_mut().item_spacing.x = 0.0;
-          // TODO: maybe use a PathBuf instead of a String?
-          // there are some problems with using a PathBuf
-          for dir in path_search.clone().split('\\') {
-            ui.label(">");
-            let mut path = path_search.clone();
-            path.truncate(path.find(dir).unwrap() + dir.len());
-            let popup_id = ui.make_persistent_id(path.clone());
-            let dir = ui.button(dir);
-            if dir.clicked() {
-              ui.memory().toggle_popup(popup_id);
-            }
-            egui::popup::popup_below_widget(ui, popup_id, &dir, |ui| {
-              ui.set_min_width(75.0);
-              if let Ok(popup_dir) = read_dir(path.clone()) {
-                for dir in popup_dir {
-                  let dir = dir.unwrap();
-                  let dir_path = dir.path();
-                  if dir.metadata().unwrap().is_dir()
-                    && ui.button(dir.file_name().to_str().unwrap()).clicked()
-                  {
-                    *current_path = dir_path;
-                  }
+      // * Breadcrumb navigation
+      ui.horizontal(|ui| {
+        ui.spacing_mut().item_spacing.x = 0.0;
+        // TODO: maybe use a PathBuf instead of a String?
+        // there are some problems with using a PathBuf
+        for dir in path_search.clone().split('\\') {
+          ui.label(">");
+          let mut path = path_search.clone();
+          path.truncate(path.find(dir).unwrap() + dir.len());
+          let popup_id = ui.make_persistent_id(path.clone());
+          let dir = ui.button(dir);
+          if dir.clicked() {
+            ui.memory().toggle_popup(popup_id);
+          }
+          egui::popup::popup_below_widget(ui, popup_id, &dir, |ui| {
+            ui.set_min_width(75.0);
+            if let Ok(popup_dir) = read_dir(path.clone()) {
+              for dir in popup_dir {
+                let dir = dir.unwrap();
+                let dir_path = dir.path();
+                if dir.metadata().unwrap().is_dir()
+                  && ui.button(dir.file_name().to_str().unwrap()).clicked()
+                {
+                  *current_path = dir_path;
                 }
               }
-            });
-          }
-        });
-        ui.end_row();
-
-        let search = ui.text_edit_singleline(path_search);
-
-        if search.changed() {
-          *current_path = std::path::PathBuf::from(path_search.clone());
-        }
-        ui.end_row();
-
-        ui.horizontal(|ui| {
-          if ui.button("Go up").clicked() {
-            *current_path = current_path.parent().unwrap().to_path_buf();
-          }
-          if ui.button("Go back").clicked() {
-            *current_path = last_path.to_path_buf();
-          }
-          if pinned_dirs.contains(current_path) {
-            if ui.button("Unpin directory").clicked() {
-              pinned_dirs.retain(|x| x != &current_path.clone());
             }
-          } else if ui.button("Pin directory").clicked() {
-            pinned_dirs.push(current_path.to_path_buf());
-          }
-        });
-        ui.end_row();
-        
-        if search.lost_focus() && ui.input().key_pressed(egui::Key::Enter)
-          || current_path != last_path
-        {
-          let dir_path = std::path::Path::new(&current_path);
-          if let Ok(dir) = read_dir(dir_path) {
-            set_current_dir(dir_path).unwrap();
-            *path_search = dir_path.to_str().unwrap().to_owned();
-            *current_path = dir_path.to_path_buf();
-            *dir_entries = Vec::new();
-
-            for entry in dir {
-              let entrypath = entry.unwrap().path();
-              let entrypath2 = entrypath.clone().into_os_string().into_string().unwrap();
-              let dir_size = match filesystem.files.get(&entrypath2) {
-                Some(dir_size) => dir_size.real_size,
-                None => 0,
-              };
-
-              dir_entries.push(DirEntry {
-                name: entrypath
-                  .clone()
-                  .file_name()
-                  .unwrap()
-                  .to_str()
-                  .unwrap()
-                  .to_owned(),
-                path: entrypath,
-                size: dir_size,
-              });
-            }
-          }
-          *last_path = current_path.clone();
+          });
         }
+      });
+      ui.end_row();
+      let search = ui.text_edit_singleline(path_search);
 
-        for entry in dir_entries {
-          let name = entry.name.clone();
-          let path = entry.path.clone();
-          let is_dir = entry.path.is_dir();
-          let dir_size = ByteSize(entry.size);
-          let label = if is_dir {
-            format!("{}/", name)
-          } else {
-            name.to_owned()
-          };
+      if search.changed() {
+        *current_path = std::path::PathBuf::from(path_search.clone());
+      }
+      ui.end_row();
 
-          // ui.horizontal(|ui| {
+      ui.horizontal(|ui| {
+        if ui.button("Go up").clicked() {
+          *current_path = current_path.parent().unwrap().to_path_buf();
+        }
+        if ui.button("Go back").clicked() {
+          *current_path = last_path.to_path_buf();
+        }
+        if pinned_dirs.contains(current_path) {
+          if ui.button("Unpin directory").clicked() {
+            pinned_dirs.retain(|x| x != &current_path.clone());
+          }
+        } else if ui.button("Pin directory").clicked() {
+          pinned_dirs.push(current_path.to_path_buf());
+        }
+      });
+      ui.end_row();
+      egui::ScrollArea::vertical().show(ui, |ui| {
+        egui::Grid::new("central_grid").show(ui, |ui| {
+          if search.lost_focus() && ui.input().key_pressed(egui::Key::Enter)
+            || current_path != last_path
+          {
+            let dir_path = std::path::Path::new(&current_path);
+            if let Ok(dir) = read_dir(dir_path) {
+              set_current_dir(dir_path).unwrap();
+              *path_search = dir_path.to_str().unwrap().to_owned();
+              *current_path = dir_path.to_path_buf();
+              *dir_entries = Vec::new();
+
+              for entry in dir {
+                let entrypath = entry.unwrap().path();
+                let entrypath2 = entrypath.clone().into_os_string().into_string().unwrap();
+                let dir_size = match filesystem.files.get(&entrypath2) {
+                  Some(dir_size) => dir_size.real_size,
+                  None => 0,
+                };
+
+                dir_entries.push(DirEntry {
+                  name: entrypath
+                    .clone()
+                    .file_name()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+                    .to_owned(),
+                  path: entrypath,
+                  size: dir_size,
+                });
+              }
+            }
+            *last_path = current_path.clone();
+          }
+
+          for entry in dir_entries {
+            let name = entry.name.clone();
+            let path = entry.path.clone();
+            let is_dir = entry.path.is_dir();
+            let dir_size = ByteSize(entry.size);
+            let label = if is_dir {
+              format!("{}/", name)
+            } else {
+              name.to_owned()
+            };
+
+            // ui.horizontal(|ui| {
             if ui.button(&label).clicked() {
               if is_dir {
                 *current_path = path.to_path_buf()
@@ -286,9 +285,10 @@ impl epi::App for App {
             }
             ui.label(label);
             ui.label(dir_size.to_string());
-          // });
-          ui.end_row();
-        }
+            // });
+            ui.end_row();
+          }
+        });
       });
     });
     if false {
