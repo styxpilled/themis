4c3bc3c17e3fb0a0d36470e301ddb7b2cc167f35
styxpilled <styxpilled.dev@gmail.com>
2022-04-07 22:00:51 +0200

    refactor: split file menu into file

---
+++ src/ui/file_menu.rs
@@ -0,0 +1,82 @@
+use crate::app::Themis;
+use bytesize::ByteSize;
+use eframe::egui;
+
+use crate::ui::update_current_dir;
+
+pub fn file_menu(state: &mut Themis, ui: &mut egui::Ui) {
+  for entry in state.dir_entries.clone() {
+    ui.horizontal(|ui| {
+      let name = entry.name.clone();
+      let path = entry.path.clone();
+      let is_dir = entry.path.is_dir();
+      let dir_size = ByteSize(entry.size);
+      let label = if is_dir {
+        format!("{}/", name)
+      } else {
+        name.to_owned()
+      };
+      let formatted = if is_dir {
+        if entry.is_empty {
+          format!("🗁 {} ({})", label, dir_size)
+        } else {
+          format!("🗀 {} ({})", label, dir_size)
+        }
+      } else {
+        format!("🗋 {} ({})", label, dir_size)
+      };
+      if state.rename.target.clone().unwrap_or_default() != path {
+        let thing = ui.add(egui::Label::new(formatted).sense(egui::Sense::click()));
+        if thing.double_clicked() {
+          if is_dir {
+            state.current_path = path.to_path_buf()
+          } else {
+            open::that(path.to_str().unwrap()).unwrap()
+          }
+        }
+        if thing.hovered() {
+          state.selected_path = path.to_path_buf();
+        }
+        thing.context_menu(|ui| {
+          // ui.spacing_mut().item_spacing.y = 6.0;
+          if ui.button("Print Name").clicked() {
+            println!("{:?}", state.selected_path);
+          }
+          if ui.button("Rename").clicked() {
+            state.rename.target = Some(state.selected_path.clone());
+            ui.close_menu();
+          }
+          // ui.spacing_mut().item_spacing.y = 1.5;
+        });
+      } else {
+        let rename_bar = ui.text_edit_singleline(&mut state.rename.value);
+        if rename_bar.lost_focus() {
+          if state.rename.value != "" {
+            let new_name = state.rename.value.clone();
+            let new_path = state
+              .rename
+              .target
+              .clone()
+              .unwrap()
+              .with_file_name(new_name);
+            std::fs::rename(state.rename.target.clone().unwrap(), new_path).unwrap();
+            update_current_dir(state);
+          }
+          state.rename.target = None;
+          state.rename.value = String::new();
+        } else {
+          rename_bar.request_focus();
+        }
+        if rename_bar.gained_focus() {
+          state.rename.value = name.to_owned();
+        }
+      }
+    });
+    ui.end_row();
+    ui.add(egui::Separator::spacing(
+      egui::Separator::horizontal(egui::Separator::default()),
+      0.0,
+    ));
+    ui.end_row();
+  }
+}
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,8 +1,10 @@
 use crate::app::{DirEntry, DirWatcherEvent, Themis};
-use bytesize::ByteSize;
 use eframe::egui;
 use std::env::set_current_dir;
 use std::fs::read_dir;
+
+mod file_menu;
+use file_menu::file_menu;
 
 pub fn main(ctx: &egui::Context, state: &mut Themis) {
   if state.filesystem.files.is_empty() {
@@ -118,8 +120,6 @@ pub fn main(ctx: &egui::Context, state: &mut Themis) {
       //   std::fs::File::create(new_file_path).unwrap();
       // }
     });
-    // ui.end_row();
-    // ui.text_edit_singleline(&mut state.rename_bar);
     ui.end_row();
     egui::ScrollArea::vertical().show(ui, |ui| {
       egui::Grid::new("central_grid").show(ui, |ui| {
@@ -131,80 +131,8 @@ pub fn main(ctx: &egui::Context, state: &mut Themis) {
         ui.end_row();
         ui.spacing_mut().item_spacing.y = 1.5;
         ui.vertical(|ui| {
-          for entry in state.dir_entries.clone() {
-            ui.horizontal(|ui| {
-              let name = entry.name.clone();
-              let path = entry.path.clone();
-              let is_dir = entry.path.is_dir();
-              let dir_size = ByteSize(entry.size);
-              let label = if is_dir {
-                format!("{}/", name)
-              } else {
-                name.to_owned()
-              };
-              let formatted = if is_dir {
-                if entry.is_empty {
-                  format!("🗁 {} ({})", label, dir_size)
-                } else {
-                  format!("🗀 {} ({})", label, dir_size)
-                }
-              } else {
-                format!("🗋 {} ({})", label, dir_size)
-              };
-              if state.rename.target.clone().unwrap_or_default() != path {
-                let thing = ui.add(egui::Label::new(formatted).sense(egui::Sense::click()));
-                if thing.double_clicked() {
-                  if is_dir {
-                    state.current_path = path.to_path_buf()
-                  } else {
-                    open::that(path.to_str().unwrap()).unwrap()
-                  }
-                }
-                if thing.hovered() {
-                  state.selected_path = path.to_path_buf();
-                }
-                thing.context_menu(|ui| {
-                  // ui.spacing_mut().item_spacing.y = 6.0;
-                  if ui.button("Print Name").clicked() {
-                    println!("{:?}", state.selected_path);
-                  }
-                  if ui.button("Rename").clicked() {
-                    state.rename.target = Some(state.selected_path.clone());
-                    ui.close_menu();
-                  }
-                  // ui.spacing_mut().item_spacing.y = 1.5;
-                });
-              } else {
-                let rename_bar = ui.text_edit_singleline(&mut state.rename.value);
-                if rename_bar.lost_focus() {
-                  if state.rename.value != "" {
-                    let new_name = state.rename.value.clone();
-                    let new_path = state
-                      .rename
-                      .target
-                      .clone()
-                      .unwrap()
-                      .with_file_name(new_name);
-                    std::fs::rename(state.rename.target.clone().unwrap(), new_path).unwrap();
-                    update_current_dir(state);
-                  }
-                  state.rename.target = None;
-                  state.rename.value = String::new();
-                } else {
-                  rename_bar.request_focus();
-                }
-                if rename_bar.gained_focus() {
-                  state.rename.value = name.to_owned();
-                }
-              }
-            });
-            ui.end_row();
-            ui.add(egui::Separator::spacing(
-              egui::Separator::horizontal(egui::Separator::default()),
-              0.0,
-            ));
-            ui.end_row();
-          }
+          // * Current directory file menu
+          file_menu(state, ui);
         })
         .response
         .context_menu(|ui| {
